699af2d016fa67c1cd59d33dfe07067174ed01db
mshamir11 <mshamir11@gmail.com>
2023-05-02 13:23:44 +0530

    Setup backend

---
+++ community.ride.backend/src/test/java/com/vynce/community/ride/backend/CommunityRideBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vynce.community.ride.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityRideBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
